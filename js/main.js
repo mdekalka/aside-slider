e24ad4f557afcdc8824af8f332ee4d75254ed6a1
Aleh_Isakau <Aleh_Isakau@epam.com>
2014-05-14 14:06:32 +0300

    add prev/next buttons plus prev algoritm with bug

--- js/main.js
+++ js/main.js
@@ -4,20 +4,46 @@ $(document).ready(function() {
 	//==========================================
 	//variables:
 	var content_item = $('.asideSlider-container'),
-			$links = $('.asideSlider-nav').find('li'),
+			$links = $('.asideSlider-pagination').find('li'),
+			prev = $('.asideSlider-left'),
+			next = $('.asideSlider-right'),
 			item_position,
 			index,
-			elementsNumber = $links.length;
+			elementsNumber = $links.length,
+			currentState = 0,
+			currentLink,
+			nextLink;
 
 
 	//click event on links
-	$('.asideSlider-nav').on('click', 'a', function(e) {
+	$('.asideSlider-pagination').on('click', 'a', function(e) {
 		e.preventDefault();
 		var $this = $(this)
 				state = $this.closest('li').index();
 		//===========================
 		showNext($this, state);
 		
+	});
+
+	//hover on main content:
+	content_item.hover(function() {
+		/* Stuff to do when the mouse enters the element */
+		clearInterval(timer);
+	}, function() {
+		/* Stuff to do when the mouse leaves the element */
+		timer = setInterval(checkStateNext, 5000);
+	});
+
+
+	//prev button click
+	prev.on('click', function(e) {
+		e.preventDefault();
+		checkStatePrev();
+	});
+	//next button click
+	next.on('click', function(e) {
+		e.preventDefault();
+		checkStateNext();
 	});
 
 	//show another part of content	
@@ -37,24 +63,11 @@ $(document).ready(function() {
 			.fadeOut('100')
 			.removeClass('active');
 	};
-
-	function checkState() {
-		var currentState = 0,
-				currentLink,
-				nextLink;
-		//==========================
+	//check current state of page and move to right
+	function checkStateNext() {
 		currentState = $links.filter('.active').index();
 		currentLink = $links.filter('.active');
 
-		// if (currentLink.is(':last-child')) {
-		// 	nextLink = $links.first();
-		// 	console.log('last');
-		// } else if (currentLink.is(':first-child')) {
-		// 	nextLink = currentLink;
-		// 	console.log('first');
-		// } else {
-		// 	nextLink = currentLink.next();
-		// }
 		if (currentState < elementsNumber - 1) {
 			currentState++;
 			nextLink = currentLink.next();
@@ -62,11 +75,24 @@ $(document).ready(function() {
 			currentState = 0;
 			nextLink = $links.first();
 		};
-
 		showNext(nextLink, currentState);
 
 	};
-	setInterval(checkState, 5000);
+	function checkStatePrev() {
+		currentState = $links.filter('.active').index();
+		currentLink = $links.filter('.active');
+		if (currentState) {
+			currentState--;
+			nextLink = currentLink.prev();
+		} else {
+			currentLink = 3;
+			nextLink = $links.filter(':last');
+			console.log('last');
+		};
+		showNext(nextLink, currentState);
+	};
+	//run autoplay of slider
+	var timer = setInterval(checkStateNext, 5000);
 
 
 });
